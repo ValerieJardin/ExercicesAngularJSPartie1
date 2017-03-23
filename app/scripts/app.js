10177e887b54ffb86338e397be9c8d9afa780f43
Valerie Jardin <valerie.jardin60@gmail.com>
2017-03-23 12:02:18 +0100

    Partie 1 d'AngularJS faite

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name exercice1AngularJsApp
+ * @description
+ * # exercice1AngularJsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('exercice1AngularJsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exercice1AngularJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the exercice1AngularJsApp
+ */
+angular.module('exercice1AngularJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
